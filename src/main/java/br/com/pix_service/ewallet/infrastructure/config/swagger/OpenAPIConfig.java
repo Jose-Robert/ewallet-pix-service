3e6b5850895d918e7becf6a709b409c0489f5cac
José Robert <robert.suporteweb@gmail.com>
2025-11-30 10:54:01 -0300

    [REFACTOR] - Adiciona ajustes no swagger

--- src/main/java/br/com/pix_service/ewallet/infrastructure/config/swagger/OpenAPIConfig.java
+++ src/main/java/br/com/pix_service/ewallet/infrastructure/config/swagger/OpenAPIConfig.java
@@ -1,6 +1,7 @@
 package br.com.pix_service.ewallet.infrastructure.config.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,14 @@ public class OpenAPIConfig {
                 .info(new Info()
                         .title("Wallet API - Pix Service")
                         .description("Microserviço de carteira com suporte a Pix, garantindo consistência sob concorrência e idempotência.")
+                        .termsOfService("Terms of service")
+                        .contact(new Contact()
+                                .name("DEVELOPER IT")
+                                .url("https://github.com/Jose-Robert")
+                                .email("jrobert.dev@hotmail.com"))
+                        .license(new io.swagger.v3.oas.models.info.License()
+                                .name("License of API")
+                                .url("API license URL"))
                         .version("1.0.0"));
     }
 }
